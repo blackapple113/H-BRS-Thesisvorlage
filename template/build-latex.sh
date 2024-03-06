c94d5155095f4ba24759293a014ae01a113d4dc4
Thomas Schüller <30127149+blackapple113@users.noreply.github.com>
2024-03-06 18:20:16 +0100

    updated Dockerfiles for usage of new version

--- template/build-latex.sh
+++ template/build-latex.sh
@@ -3,15 +3,15 @@
 file=Document.tex
 
 # check if there is an argument
-if !([ -z "$1" ]); then
+if [ -n "$1" ]; then
     file=$1
 fi
 
-echo Compiling $file
+echo Compiling "$file"
 # compile latex files
-latexmk -pdf -interaction=nonstopmode -file-line-error -outdir=./out/ -shell-escape $file
+latexmk -pdf -interaction=nonstopmode -file-line-error -outdir=./out/ -shell-escape "$file"
 
 chown -R latex:latex out
 
 # copy compiled file in main repository for convenience
-cp -f ./out/${file%.*}.pdf .
+cp -f ./out/"${file%.*}".pdf .
